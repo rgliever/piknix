0198c1f53066bc913a8a6b631622eac8aac4922e
Ryan Gliever <ryangliever@gmail.com>
2015-09-17 14:26:39 -0700

    Users with update, delete account in settings; might return to users later to add more

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
   		# LOG IN
   		log_in user
   		params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-  		redirect_to user
+  		redirect_back_or user
   	else
   		flash.now[:error] = "Invalid name/password combination."
   		render 'new'
