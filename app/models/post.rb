01527839feeb3294a70f4553a458411a3e31203b
Ryan Gliever <ryangliever@gmail.com>
2015-09-17 14:48:33 -0700

    Added .gifv to post regex validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
 	belongs_to :user
-	VALID_URL_REGEX = /\A[\w+\-.:\/]+\.[\w+\-.]+[\/\w+-.]*\/[\w+\-.]+.(jpg|jpeg|png|gif)\z/i
+	VALID_URL_REGEX = /\A[\w+\-.:\/]+\.[\w+\-.]+[\/\w+-.]*\/[\w+\-.]+.(jpg|jpeg|png|gif|gifv)\z/i
 	validates :url, presence: true, format: { with: VALID_URL_REGEX }
 	validates :description, length: { maximum: 150 }
 end
